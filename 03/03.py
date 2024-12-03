d189cd15fc8e64c4ebcdbd84e56a3272f939d6b6
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2024-12-03 16:09:29 -0600

    day 3 part 1 completed

--- 03/03.py
+++ 03/03.py
@@ -1,21 +1,28 @@
+import re
 
-
-def process_input(path: str) -> list[str]:
+def process_input(path: str) -> str: 
     ''' Reads the puzzle input. '''
     with open(path) as f:
-        return f.readlines()
+        return f.read()
 
-def part_1(puzzle_input:list[str]) -> int:
+def part_1(puzzle_input:str) -> int:
     ''' Solves for Part 1. '''
-    return 0
+
+    matches = re.findall(r'mul\((\d+),(\d+)\)', puzzle_input)
+    products = []
+    for match in matches:
+        products.append(int(match[0]) * int(match[1]))
+
+    return sum(products)
+    
 
 def part_2(puzzle_input:list[str]) -> int:
     ''' Solves for Part 2. '''
     return 0
 
 if __name__ == '__main__':
-    puzzle_input = process_input('03/test_input.txt')
-    # puzzle_input = process_input('03/input.txt')
+    # puzzle_input = process_input('03/test_input.txt')
+    puzzle_input = process_input('03/input.txt')
     print("Part 1: " + str(part_1(puzzle_input)))
 
     print("Part 2: " + str(part_2(puzzle_input)))
