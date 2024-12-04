c556aeb11ae93a6b7b375d50854d3237bf836976
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2024-12-04 11:29:38 -0600

    input parsing - forward, back, top-to-bot, and bot-to-top tested and working.

--- 04/04.py
+++ 04/04.py
@@ -3,8 +3,33 @@ import sys
 
 def process_input(path: str) -> list[str]:
     ''' Reads the puzzle input. '''
+    forwards = []
+    backwards = []
+    vert_tb = []
+    vert_bt = []
+    diag_trbl = []
+    diag_bltr = []
+    diag_tlbr = []
+    diag_brtl = []
+
     with open(path) as f:
-        return f.readlines()
+        raw = [line.strip() for line in f.readlines()]
+        forwards = [x for x in raw]
+        backwards = [x[::-1] for x in raw]
+        vert_tb = []
+        for col_num in range(len(raw[0])):
+            tmp = ''
+            for row_num in range(len(raw)):
+                tmp += raw[row_num][col_num]
+            vert_tb.append(tmp)
+        vert_bt = [x[::-1] for x in vert_tb]
+
+        print(f'forward: {forwards}')
+        print(f'backward: {backwards}')
+        print(f'vert_tb: {vert_tb}')
+        print(f'vert_bt: {vert_bt}')
+
+    return None
 
 def part_1(puzzle_input:list[str]) -> int:
     ''' Solves for Part 1. '''
