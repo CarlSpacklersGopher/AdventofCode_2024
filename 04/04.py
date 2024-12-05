9d0b2f6dfa5806d5adc996a850a48df596ba59d2
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2024-12-05 00:54:34 -0600

    all the diagonals appear to be working

--- 04/04.py
+++ 04/04.py
@@ -28,49 +28,53 @@ def process_input(path: str) -> list[str]:
 
         # Diagonals
         for diag_slice_num in range(len(raw[0])): # Assume square
+            # Top Right to Bottom Left
             l_trbl_slice = ''
             r_trbl_slice = ''
             l_trbl_slice_start = (0, len(raw[0]) - 1 - diag_slice_num)
             r_trbl_slice_start = (0 + diag_slice_num, len(raw[0]) - 1)
 
+            # Top Left to Bottom Right 
+            l_tlbr_slice = ''
+            r_tlbr_slice = ''
+            l_tlbr_slice_start = (0 + diag_slice_num, 0)
+            r_tlbr_slice_start = (0, 0 + diag_slice_num)
+
             count = 0
             slice_complete = False
             while not slice_complete:
                 if count > len(raw[0]): # max traversal distance is diagonal length
-                    print('TR -> BL (L): ' + l_trbl_slice)
-                    print('TR -> BL (R): ' + r_trbl_slice)
                     slice_complete = True
+
+                # Top Right to Bottom Left
                 if l_trbl_slice_start[1] - count >= 0:
                     l_trbl_slice += raw[l_trbl_slice_start[0] + count][l_trbl_slice_start[1] - count]
                 if r_trbl_slice_start[0] + count < len(raw):
                     r_trbl_slice += raw[r_trbl_slice_start[0] + count][r_trbl_slice_start[1] - count]
+
+                # Top Left to Bottom Right
+                if l_tlbr_slice_start[0] + count < len(raw):
+                    l_tlbr_slice += raw[l_tlbr_slice_start[0] + count][l_tlbr_slice_start[1] + count]
+                if r_tlbr_slice_start[1] + count < len(raw[0]):
+                    r_tlbr_slice += raw[r_tlbr_slice_start[0] + count][r_tlbr_slice_start[1] + count]
                 count += 1
 
-        '''
-        for col_num in range(len(raw[0])):
-            tmp_trbl = '' 
-            tmp_tlbr = ''
+            diag_trbl.append(l_trbl_slice)
+            diag_trbl.append(r_trbl_slice)
 
-            count = 0
-            while True:
-                if count > len(raw[0]):
-                    break
-                if col_num - count >= 0:
-                    tmp_trbl += raw[count][col_num - count]
-                if col_num + count < len(raw[0]):
-                    tmp_tlbr += raw[count][col_num + count]
-                count += 1
-            
-            print(f'TR -> BL: {tmp_trbl}')
-            print(f'TL -> BR: {tmp_tlbr}')
-            diag_trbl.append(tmp_trbl)
-            diag_tlbr.append(tmp_tlbr)
-        '''
+            diag_tlbr.append(l_tlbr_slice)
+            diag_tlbr.append(r_tlbr_slice)
 
+        diag_bltr = [x[::-1] for x in diag_trbl]
+        diag_brtl = [x[::-1] for x in diag_tlbr]        
 
-
-
-    return None
+    return [forwards,
+            backwards,
+            vert_tb,
+            vert_bt,
+            diag_trbl,
+            diag_bltr,
+            diag_tlbr]
 
 def part_1(puzzle_input:list[str]) -> int:
     ''' Solves for Part 1. '''
