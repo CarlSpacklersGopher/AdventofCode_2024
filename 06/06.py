edd0e50e610948a9e2613b14fade993a43b2236f
CarlSpacklersGopher <43297884+CarlSpacklersGopher@users.noreply.github.com>
2024-12-06 11:09:40 -0600

    input parsing complete

--- 06/06.py
+++ 06/06.py
@@ -1,10 +1,20 @@
 
 import sys
 
-def process_input(path: str) -> list[str]:
+def process_input(path: str) -> tuple[tuple[int, int], list[list[str]]]:
     ''' Reads the puzzle input. '''
+    guard = (-1, -1)
     with open(path) as f:
-        return f.readlines()
+        content = f.read()
+        lines = content.split()
+
+        cols = len(lines[0]) + 1 # +1 to account for \n
+        g = content.find('^')
+        guard = (g % cols, g // cols)
+
+        grid = [list(l) for l in lines]
+
+        return (guard, grid)
 
 def part_1(puzzle_input:list[str]) -> int:
     ''' Solves for Part 1. '''
@@ -19,4 +29,4 @@ if __name__ == '__main__':
     print("Part 1: " + str(part_1(puzzle_input)))
 
     print("Part 2: " + str(part_2(puzzle_input)))
-    
+
